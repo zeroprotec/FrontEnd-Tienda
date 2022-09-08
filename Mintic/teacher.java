62d8a2c6a8f26f8446d9ea37961e0ec7c4846cdc
zeroprotec <zero.protec@gmail.com>
2022-09-08 16:48:47 -0500

    Carpeta de pruebas zeroprotec Creada

--- Mintic/teacher.java
+++ Mintic/teacher.java
@@ -1,20 +1,20 @@
-package Mintic;
-import javax.persistence
-import javax.annotation.processing.Generated;
+// package Mintic;
+// import javax.persistence;
+// import javax.annotation.processing.Generated;
 
-import java.lang.annotation.Inherited;
-import java.util.Date;
+// import java.lang.annotation.Inherited;
+// import java.util.Date;
 
-@Getter
-@Setter
-@Entity
-@table(name=teacher)
-public class teacher {
-    // Atributos
-    @Id
-    @GeneratedValue(strategy );
-    private int code;
-    private String name;
-    private String phone;
-    private String email;
-}
+// @Getter
+// @Setter
+// @Entity
+// @table(name=teacher)
+// public class teacher {
+//     // Atributos
+//     @Id
+//     @GeneratedValue(strategy );
+//     private int code;
+//     private String name;
+//     private String phone;
+//     private String email;
+// }
