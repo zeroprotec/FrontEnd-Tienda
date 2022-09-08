9cb27fdd32d05687c1cfe39d74cf1f40045fca61
zeroprotec <zero.protec@gmail.com>
2022-09-08 16:47:57 -0500

    Preba conexion db con PHP

---
+++ db/registro-sing-in.php
@@ -0,0 +1,22 @@
+<?php
+
+// $conex=@mysqli_connect("localhost","root","","mitiendavirtual");
+// if (!$conex) {
+//     echo"No se pudo conectar al servidor";
+// }else{
+//     echo"Conexion exitosa";
+// }
+
+// $nombre=$_POST["nombre"];
+// $apellido=$_POST["apellido"];
+// $email=$_POST["email"];
+// $indicativo=$_POST["indicativo"];
+// $telefono=$_POST["telefono"];
+// $contraseña=$_POST["contraseña"];
+// $sql="INSERT INTO usuario VALUES('$nombre',
+//                                  '$email',
+//                                  '$telefono',
+//                                  '$contraseña')";
+// $ejecutar = mysql_query("sql",$sql);
+
+?>
